c6840bdcc7e4feaaa255e76f6cce5e25c5c7cdf1
Humayra Jahan <37900722+hjhimika@users.noreply.github.com>
2024-10-07 12:41:26 +0600

    Create Transcribtion_Translation.py
    Trancrib and translate to language

---
+++ Transcribtion_Translation.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+from docx import Document
+client = OpenAI(api_key="...")
+with open("/content/Japanese Listening Practice.mp3", "rb") as audio_file:
+    transcript_es = client.audio.transcriptions.create(
+        file = audio_file,
+        model = "whisper-1",
+        response_format="text",
+        language="ja"
+    )
+print(transcript_es)
+print("translation:")
+
+
+with open("/content/Japanese Listening Practice.mp3", "rb") as audio_file:
+    translation = client.audio.translations.create(
+        file = audio_file,
+        model = "whisper-1",
+        response_format="text"
+    )
+print(translation)
